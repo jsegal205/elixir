775363fd49f34096f73f5d8c45e48a2c1f9709a2
Jim Segal <hello@jimsegal.com>
2021-03-13 17:04:32 -0600

    wire up add and subtract buttons, add documentation links

--- live_view_counter/lib/live_view_counter_web/live/counter_live.ex
+++ live_view_counter/lib/live_view_counter_web/live/counter_live.ex
@@ -1,14 +1,34 @@
 defmodule LiveViewCounterWeb.CounterLive do
   use Phoenix.LiveView
 
+  # https://hexdocs.pm/phoenix_live_view/Phoenix.LiveView.html?#c:mount/3
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :count, 23)}
+    # https://hexdocs.pm/phoenix_live_view/Phoenix.LiveView.html#assign/3
+    {:ok, assign(socket, :count, 0)}
   end
 
+  # https://hexdocs.pm/phoenix_live_view/Phoenix.LiveView.html#c:handle_event/3
+  def handle_event("add", _, socket) do
+    # https://hexdocs.pm/phoenix_live_view/Phoenix.LiveView.html#update/3
+    # https://dockyard.com/blog/2016/08/05/understand-capture-operator-in-elixir
+    {:noreply, update(socket, :count, &(&1 + 1))}
+  end
+
+  # https://hexdocs.pm/phoenix_live_view/Phoenix.LiveView.html#c:handle_event/3
+  def handle_event("subtract", _, socket) do
+    # https://hexdocs.pm/phoenix_live_view/Phoenix.LiveView.html#update/3
+    # https://dockyard.com/blog/2016/08/05/understand-capture-operator-in-elixir
+    {:noreply, update(socket, :count, &(&1 - 1))}
+  end
+
+  # https://hexdocs.pm/phoenix_live_view/Phoenix.LiveView.html#c:handle_event/3
   def render(assigns) do
+    # https://hexdocs.pm/phoenix_live_view/Phoenix.LiveView.Helpers.html#sigil_L/2
     ~L"""
     <div>
       <label> The count is <%= assigns[:count] %></label>
+      <button phx-click="add">Add</button>
+      <button phx-click="subtract">Subtract</button>
     </div>
     """
   end
