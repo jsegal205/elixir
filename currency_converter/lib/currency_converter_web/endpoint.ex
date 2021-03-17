e9b5b51e2c4ad415c6b59497f9729ff1e0b38f96
Jim Segal <hello@jimsegal.com>
2021-03-17 17:42:47 -0500

    remove favico noise

--- currency_converter/lib/currency_converter_web/endpoint.ex
+++ currency_converter/lib/currency_converter_web/endpoint.ex
@@ -24,7 +24,7 @@ defmodule CurrencyConverterWeb.Endpoint do
     at: "/",
     from: :currency_converter,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js  robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
