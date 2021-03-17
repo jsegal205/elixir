5e9145178d8d52438dd0d779d3d27fd5177e8733
Jim Segal <hello@jimsegal.com>
2021-03-17 18:23:27 -0500

    add null param handling

--- currency_converter/lib/currency_converter_web/controllers/currency_converter_controller.ex
+++ currency_converter/lib/currency_converter_web/controllers/currency_converter_controller.ex
@@ -1,7 +1,14 @@
 defmodule CurrencyConverterWeb.CurrencyController do
   use CurrencyConverterWeb, :controller
 
-  def index(conn, _params) do
+  def index(conn, %{"from" => from, "to" => to, "amount" => amount} = params) do
     json(conn, %{success: true})
+  end
+
+  def index(conn, _params) do
+    json(conn, %{
+      success: false,
+      message: "Please provide parameters for conversion: `from`, `to`, and `amount`"
+    })
   end
 end
