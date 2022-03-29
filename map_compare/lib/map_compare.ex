51dcda21e0ab9194aaa901108af0235241f83648
Jim Segal <hello@jimsegal.com>
2022-03-28 22:45:07 -0500

    switch from doc tests, add easy use cases

--- map_compare/lib/map_compare.ex
+++ map_compare/lib/map_compare.ex
@@ -1,18 +1,18 @@
 defmodule MapCompare do
   @moduledoc """
-  Documentation for `MapCompare`.
+  You have 2 maps. You are looking for the difference between the 2. What was added or removed or if the map is the same.
+
+  Map only have string keys
+  Map only have string, boolean, number, list or map as value
+  Compare should have an option for deep or shallow compare
+  Compare should list the difference for keys and values
   """
 
-  @doc """
-  Hello world.
+  def compare(a, b) when not is_map(a) or not is_map(b) do
+    {:error, "invalid arg type"}
+  end
 
-  ## Examples
-
-      iex> MapCompare.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def compare(a = %{}, a = %{}) do
+    %{same: true, added_to_b: %{}, removed_from_a: %{}}
   end
 end
--- map_compare/test/map_compare_test.exs
+++ map_compare/test/map_compare_test.exs
@@ -1,8 +1,39 @@
 defmodule MapCompareTest do
   use ExUnit.Case
-  doctest MapCompare
 
-  test "greets the world" do
-    assert MapCompare.hello() == :world
+  test "when both args are not maps" do
+    assert MapCompare.compare(1, 1) == {:error, "invalid arg type"}
+    assert MapCompare.compare(1, "two") == {:error, "invalid arg type"}
+    assert MapCompare.compare(1, true) == {:error, "invalid arg type"}
+    assert MapCompare.compare(1, []) == {:error, "invalid arg type"}
+    assert MapCompare.compare(1, %{}) == {:error, "invalid arg type"}
+    assert MapCompare.compare("one", 1) == {:error, "invalid arg type"}
+    assert MapCompare.compare("one", "two") == {:error, "invalid arg type"}
+    assert MapCompare.compare("one", true) == {:error, "invalid arg type"}
+    assert MapCompare.compare("one", []) == {:error, "invalid arg type"}
+    assert MapCompare.compare("one", %{}) == {:error, "invalid arg type"}
+    assert MapCompare.compare(false, 1) == {:error, "invalid arg type"}
+    assert MapCompare.compare(false, "two") == {:error, "invalid arg type"}
+    assert MapCompare.compare(false, true) == {:error, "invalid arg type"}
+    assert MapCompare.compare(false, []) == {:error, "invalid arg type"}
+    assert MapCompare.compare(false, %{}) == {:error, "invalid arg type"}
+    assert MapCompare.compare([], 1) == {:error, "invalid arg type"}
+    assert MapCompare.compare([], "two") == {:error, "invalid arg type"}
+    assert MapCompare.compare([], true) == {:error, "invalid arg type"}
+    assert MapCompare.compare([], []) == {:error, "invalid arg type"}
+    assert MapCompare.compare([], %{}) == {:error, "invalid arg type"}
+    assert MapCompare.compare(%{}, 1) == {:error, "invalid arg type"}
+    assert MapCompare.compare(%{}, "two") == {:error, "invalid arg type"}
+    assert MapCompare.compare(%{}, true) == {:error, "invalid arg type"}
+    assert MapCompare.compare(%{}, []) == {:error, "invalid arg type"}
+  end
+
+  test "when two empty maps passed" do
+    assert MapCompare.compare(%{}, %{}) == %{same: true, added_to_b: %{}, removed_from_a: %{}}
+  end
+
+  test "when to maps with same key value pairs" do
+    map_a = %{"integer" => 1, "string" => "two", "boolean" => false, "list" => [], "map" => %{}}
+    assert MapCompare.compare(map_a, map_a) == %{same: true, added_to_b: %{}, removed_from_a: %{}}
   end
 end
