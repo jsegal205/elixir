ed4207ae9dc1200998726418780a90e017ad2877
Jim Segal <hello@jimsegal.com>
2022-03-28 22:21:06 -0500

    init commit
    mix new map_compare

---
+++ map_compare/lib/map_compare.ex
@@ -0,0 +1,18 @@
+defmodule MapCompare do
+  @moduledoc """
+  Documentation for `MapCompare`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MapCompare.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ map_compare/test/map_compare_test.exs
@@ -0,0 +1,8 @@
+defmodule MapCompareTest do
+  use ExUnit.Case
+  doctest MapCompare
+
+  test "greets the world" do
+    assert MapCompare.hello() == :world
+  end
+end
