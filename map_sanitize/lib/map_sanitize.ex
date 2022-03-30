3fb157f040be98b285b0cb660bca6efe1f695237
Jim Segal <hello@jimsegal.com>
2022-03-29 20:07:10 -0500

    init commit
    mix new map_sanitize

---
+++ map_sanitize/lib/map_sanitize.ex
@@ -0,0 +1,18 @@
+defmodule MapSanitize do
+  @moduledoc """
+  Documentation for `MapSanitize`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MapSanitize.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ map_sanitize/test/map_sanitize_test.exs
@@ -0,0 +1,8 @@
+defmodule MapSanitizeTest do
+  use ExUnit.Case
+  doctest MapSanitize
+
+  test "greets the world" do
+    assert MapSanitize.hello() == :world
+  end
+end
