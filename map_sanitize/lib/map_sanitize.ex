34df91ac0af634832bb557647b329923f972f831
Jim Segal <hello@jimsegal.com>
2022-03-29 21:55:08 -0500

    add lib and tests to support qualifications

--- map_sanitize/lib/map_sanitize.ex
+++ map_sanitize/lib/map_sanitize.ex
@@ -1,18 +1,39 @@
 defmodule MapSanitize do
   @moduledoc """
-  Documentation for `MapSanitize`.
+  A library for sanitizing potentially sensitive data that may be passed in as a source
   """
 
-  @doc """
-  Hello world.
+  @scrub_atom_keys [:name, :password, :username]
+  @scrub_string_keys ["name", "password", "username"]
+  @replace "********"
 
-  ## Examples
+  def scrub(input) when not is_map(input), do: input
 
-      iex> MapSanitize.hello()
-      :world
+  def scrub(input), do: do_scrub(input)
 
-  """
-  def hello do
-    :world
+  defp do_scrub(map, sanitized_input \\ %{}) do
+    Enum.reduce(map, sanitized_input, &scrub_map(&1, &2))
+  end
+
+  defp scrub_map({key, val}, sanitized_input) when is_list(val) do
+    Map.put(sanitized_input, key, Enum.map(val, &do_scrub(&1)))
+  end
+
+  defp scrub_map({key, val}, sanitized_input) when is_map(val) do
+    Map.put(sanitized_input, key, do_scrub(val, sanitized_input))
+  end
+
+  defp scrub_map({key, _}, sanitized_input)
+       when key in @scrub_atom_keys or key in @scrub_string_keys do
+    Map.put(sanitized_input, key, @replace)
+  end
+
+  defp scrub_map({key, val}, sanitized_input) when key in [:email, "email"] do
+    # .+?(?=@) matches everything before given @ symbol
+    Map.put(sanitized_input, key, String.replace(val, ~r/.+?(?=@)/, @replace))
+  end
+
+  defp scrub_map({key, val}, sanitized_input) do
+    Map.put(sanitized_input, key, val)
   end
 end
--- map_sanitize/test/map_sanitize_test.exs
+++ map_sanitize/test/map_sanitize_test.exs
@@ -1,8 +1,207 @@
 defmodule MapSanitizeTest do
   use ExUnit.Case
-  doctest MapSanitize
 
-  test "greets the world" do
-    assert MapSanitize.hello() == :world
+  test "returns input when not a map" do
+    assert MapSanitize.scrub(1) == 1
+    assert MapSanitize.scrub("string") == "string"
+    assert MapSanitize.scrub(true) == true
+    assert MapSanitize.scrub([]) == []
+  end
+
+  test "does not scrub keys that are not name, username, password or email" do
+    assert MapSanitize.scrub(%{id: 1}) === %{id: 1}
+    assert MapSanitize.scrub(%{"id" => 1}) === %{"id" => 1}
+  end
+
+  test "combined map input with diverse keys and values" do
+    assert MapSanitize.scrub(%{
+             id: 1,
+             name: "jim",
+             email: "jim@email.com",
+             password: "hunter2",
+             fav_fruit: "apple",
+             friends: [
+               %{name: "tom", pets_name: "doggo"},
+               %{name: "alice", pets_name: "tacocat"}
+             ]
+           }) === %{
+             id: 1,
+             name: "********",
+             email: "********@email.com",
+             password: "********",
+             fav_fruit: "apple",
+             friends: [
+               %{name: "********", pets_name: "doggo"},
+               %{name: "********", pets_name: "tacocat"}
+             ]
+           }
+
+    assert MapSanitize.scrub(%{
+             "id" => 1,
+             "name" => "jim",
+             "email" => "jim@email.com",
+             "password" => "hunter2",
+             "fav_fruit" => "apple",
+             "friends" => [
+               %{"name" => "tom", "pets_name" => "doggo"},
+               %{"name" => "alice", "pets_name" => "tacocat"}
+             ]
+           }) === %{
+             "id" => 1,
+             "name" => "********",
+             "email" => "********@email.com",
+             "password" => "********",
+             "fav_fruit" => "apple",
+             "friends" => [
+               %{"name" => "********", "pets_name" => "doggo"},
+               %{"name" => "********", "pets_name" => "tacocat"}
+             ]
+           }
+  end
+
+  test "scrubs name key" do
+    assert MapSanitize.scrub(%{name: "jim"}) === %{name: "********"}
+    assert MapSanitize.scrub(%{"name" => "jim"}) === %{"name" => "********"}
+
+    assert MapSanitize.scrub(%{
+             foo: %{bar: %{baz: %{name: "jim"}}}
+           }) === %{
+             foo: %{bar: %{baz: %{name: "********"}}}
+           }
+
+    assert MapSanitize.scrub(%{
+             "foo" => %{"bar" => %{"baz" => %{"name" => "jim"}}}
+           }) === %{
+             "foo" => %{"bar" => %{"baz" => %{"name" => "********"}}}
+           }
+  end
+
+  test "when name key is a map" do
+    assert MapSanitize.scrub(%{
+             name: %{name: "jim"}
+           }) === %{
+             name: %{name: "********"}
+           }
+  end
+
+  test "when name key is a list" do
+    assert MapSanitize.scrub(%{
+             name: [%{name: "jim"}]
+           }) === %{
+             name: [%{name: "********"}]
+           }
+  end
+
+  test "scrubs username key" do
+    assert MapSanitize.scrub(%{username: "jim_username"}) === %{username: "********"}
+    assert MapSanitize.scrub(%{"username" => "jim_username"}) === %{"username" => "********"}
+
+    assert MapSanitize.scrub(%{
+             foo: %{bar: %{baz: %{username: "jim_username"}}}
+           }) === %{
+             foo: %{bar: %{baz: %{username: "********"}}}
+           }
+
+    assert MapSanitize.scrub(%{
+             "foo" => %{"bar" => %{"baz" => %{"username" => "jim_username"}}}
+           }) === %{
+             "foo" => %{"bar" => %{"baz" => %{"username" => "********"}}}
+           }
+  end
+
+  test "when username key is a map" do
+    assert MapSanitize.scrub(%{
+             username: %{username: "jim_username"}
+           }) === %{
+             username: %{username: "********"}
+           }
+  end
+
+  test "when username key is a list" do
+    assert MapSanitize.scrub(%{
+             username: [%{username: "jim_username"}]
+           }) === %{
+             username: [%{username: "********"}]
+           }
+  end
+
+  test "scrubs password key" do
+    assert MapSanitize.scrub(%{password: "hunter2"}) === %{password: "********"}
+    assert MapSanitize.scrub(%{"password" => "hunter2"}) === %{"password" => "********"}
+
+    assert MapSanitize.scrub(%{
+             foo: %{bar: %{baz: %{password: "hunter2"}}}
+           }) === %{
+             foo: %{bar: %{baz: %{password: "********"}}}
+           }
+
+    assert MapSanitize.scrub(%{
+             "foo" => %{"bar" => %{"baz" => %{"password" => "hunter2"}}}
+           }) === %{
+             "foo" => %{"bar" => %{"baz" => %{"password" => "********"}}}
+           }
+  end
+
+  test "when password key is a map" do
+    assert MapSanitize.scrub(%{
+             password: %{password: "hunter2"}
+           }) === %{
+             password: %{password: "********"}
+           }
+  end
+
+  test "when password key is a list" do
+    assert MapSanitize.scrub(%{
+             password: [%{password: "hunter2"}]
+           }) === %{
+             password: [%{password: "********"}]
+           }
+  end
+
+  test "scrubs email key" do
+    assert MapSanitize.scrub(%{email: "jim@email.com"}) === %{email: "********@email.com"}
+    assert MapSanitize.scrub(%{"email" => "jim@email.com"}) === %{"email" => "********@email.com"}
+
+    assert MapSanitize.scrub(%{
+             foo: %{bar: %{baz: %{email: "jim@email.com"}}}
+           }) === %{
+             foo: %{bar: %{baz: %{email: "********@email.com"}}}
+           }
+
+    assert MapSanitize.scrub(%{
+             "foo" => %{"bar" => %{"baz" => %{"email" => "jim@email.com"}}}
+           }) === %{
+             "foo" => %{"bar" => %{"baz" => %{"email" => "********@email.com"}}}
+           }
+  end
+
+  test "when email key is a map" do
+    assert MapSanitize.scrub(%{
+             email: %{email: "jim@email.com"}
+           }) === %{
+             email: %{email: "********@email.com"}
+           }
+  end
+
+  test "when email key is a list" do
+    assert MapSanitize.scrub(%{
+             email: [%{email: "jim@email.com"}]
+           }) === %{
+             email: [%{email: "********@email.com"}]
+           }
+  end
+
+  test "when email does not contain @" do
+    assert MapSanitize.scrub(%{
+             email: "jim_at_email_dot_com"
+           }) === %{
+             email: "jim_at_email_dot_com"
+           }
+
+    assert MapSanitize.scrub(%{
+             "email" => "jim_at_email_dot_com"
+           }) === %{
+             "email" => "jim_at_email_dot_com"
+           }
   end
 end
