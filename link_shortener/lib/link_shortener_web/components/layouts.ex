c7e7044eb0e22f5ab1c153784dad958491f22f03
Jim Segal <hello@jimsegal.com>
2023-09-20 20:49:35 -0500

    mix phx.new link_shortener

---
+++ link_shortener/lib/link_shortener_web/components/layouts.ex
@@ -0,0 +1,5 @@
+defmodule LinkShortenerWeb.Layouts do
+  use LinkShortenerWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ link_shortener/test/link_shortener_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule LinkShortenerWeb.PageControllerTest do
+  use LinkShortenerWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
