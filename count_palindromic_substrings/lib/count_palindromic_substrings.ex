b969e34c71e661ca28845fec5d684876b74eab5a
Jim Segal <hello@jimsegal.com>
2024-10-18 14:41:03 -0500

    add count palindromic substrings impl

--- count_palindromic_substrings/lib/count_palindromic_substrings.ex
+++ count_palindromic_substrings/lib/count_palindromic_substrings.ex
@@ -3,16 +3,34 @@ defmodule CountPalindromicSubstrings do
   Documentation for `CountPalindromicSubstrings`.
   """
 
-  @doc """
-  Hello world.
+  def count(s) do
+    total_count = 0
 
-  ## Examples
+    total_count =
+      Enum.reduce(0..(String.length(s) - 1), total_count, fn i, acc ->
+        # Check for odd-length palindromes (center is at i)
+        # ex `aba`
+        acc = count_palindromes_around_center(s, i, i, acc)
 
-      iex> CountPalindromicSubstrings.hello()
-      :world
+        # Check for even-length palindromes (center is between i and i + 1)
+        # ex `abba`
+        acc = count_palindromes_around_center(s, i, i + 1, acc)
+        acc
+      end)
 
-  """
-  def hello do
-    :world
+    total_count
+  end
+
+  defp count_palindromes_around_center(s, left, right, count) do
+    if left >= 0 and right < String.length(s) and
+         String.at(s, left) == String.at(s, right) do
+      # Increment count by 1, as this is a palindrome
+
+      count = count + 1
+      # Expand outward
+      count_palindromes_around_center(s, left - 1, right + 1, count)
+    else
+      count
+    end
   end
 end
--- count_palindromic_substrings/test/count_palindromic_substrings_test.exs
+++ count_palindromic_substrings/test/count_palindromic_substrings_test.exs
@@ -1,8 +1,29 @@
 defmodule CountPalindromicSubstringsTest do
   use ExUnit.Case
-  doctest CountPalindromicSubstrings
 
-  test "greets the world" do
-    assert CountPalindromicSubstrings.hello() == :world
+  describe "count/1" do
+    test "it counts" do
+      # a,b,c,b,a,bcb,abcba
+      assert CountPalindromicSubstrings.count("abcba") == 7
+      # a,b,b,a,bb,abba
+      assert CountPalindromicSubstrings.count("abba") == 6
+
+      assert CountPalindromicSubstrings.count("a") == 1
+
+      assert CountPalindromicSubstrings.count("ab") == 2
+
+      # t,a,c,o,c,a,t,coc,acoca,tacocat
+      assert CountPalindromicSubstrings.count("tacocat") == 10
+
+      # readme test cases
+      assert CountPalindromicSubstrings.count("abc") == 3
+      assert CountPalindromicSubstrings.count("aaa") == 6
+    end
+
+    @tag :skip
+    test "1000 character string" do
+      # this bad boy takes 20+ seconds to run. need to adjust to handle this
+      assert "a" |> String.duplicate(1000) |> CountPalindromicSubstrings.count() == 500_500
+    end
   end
 end
